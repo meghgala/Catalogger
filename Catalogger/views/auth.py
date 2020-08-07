eec34a86b280e40fa94afbd89ea03bb32dd04d84
meghgala <43577392+meghgala@users.noreply.github.com>
2020-08-07 13:13:58 +0530

    linked user and business

--- Catalogger/views/auth.py
+++ Catalogger/views/auth.py
@@ -8,20 +8,44 @@ from Catalogger import app
 mod = Blueprint('auth', __name__, url_prefix='')
 
 @mod.route("/")
-def login_page():
+def register_page():
     return render_template('auth/register1.html')
 
+# REGISTER USER ------------------------------------------------------------------------------ 
 @mod.route("/register_user",methods=["POST"])
-def reg_page():
-    user_id = createStudentAccount(request.form['name'],request.form['email'],request.form['pass'])
-    print(user_id)
-    return render_template('home/index.html')
+def register_user():
+    user_id = createUserAccount(request.form['name'],request.form['email'],request.form['pass'])
+    if(user_id):
+        return render_template('home/index.html')
+    else:
+        return redirect("/")
 
-@mod.route("/changePassword")
-def change_password():
-    if 'uid' in session:
-        return render_template('auth/changepassword.html',name=session['name'])
-    return render_template('auth/login.html')
+# REGISTER BUSINESS USER ---------------------------------------------------------------------- 
+@mod.route("/register_business_user",methods=["POST"])
+def register_business_user():
+    business_user_id = createBusinessUserAccount(request.form['name'],request.form['email'],request.form['pass'],request.form['bname'],request.form['category'])
+    if(business_user_id):
+        return render_template('home/index.html')
+    else:
+        return redirect("/")
+
+@mod.route("/loginPage")
+def login_page():
+    return render_template('auth/login1.html')
+
+@mod.route("/registerPage")
+def registerPage():
+    return render_template('auth/register1.html')
+
+# USER LOGIN ---------------------------------------------------------------------------------- 
+@mod.route("/login_user",methods=["POST"])
+def login_user():
+    login_id = loginUserAccount(request.form['email'],request.form['pass'])
+    if(login_id):
+        return render_template('home/index.html')
+    else:
+        flash('Invalid credentials', 'danger')
+        return redirect("/loginPage")
 
 @mod.route("/changePass",methods=["POST"])
 def change_pass():
@@ -34,22 +58,6 @@ def change_pass():
             return render_template('auth/changepassword.html',name=session['name'])
     flash('Please Login','danger')
     return render_template('auth/login.html')
-
-@mod.route("/loginUser",methods=["POST"])
-def login_user():
-    session.pop('uid',None)
-    userData = loginUser(request.form['email'],request.form['password'])
-    if userData:
-        session['category'],session['uid'],session['name'] = userData['category'],userData['uid'],userData['name']
-        try:
-            _ = request.form['rememberMe']
-            session.permanent = True
-        except:
-            session.permanent = False
-        return redirect('/home')
-    flash('Incorrect Credentials','danger')
-    print("hello")
-    return redirect('/')
 
 @mod.route("/logout")
 def logout_user():
--- helpers/auth.py
+++ helpers/auth.py
@@ -10,19 +10,56 @@ firebase_admin.initialize_app(cred)
 
 firestore_client = firestore.client()
 
-def createStudentAccount(name, email_id, password):
-    user = auth.create_user(
-        display_name=name,
-        email=email_id,
-        email_verified=True,
-        password=password
-    )
-    firestore_client.collection('businesses').document(user.uid).set({
-        'name': name,
-        'uid': user.uid
-    })
-    print("Created user", user.uid)
-    return user.uid
-     
+def createUserAccount(name, email_id, password):
+    try:
+        user = auth.create_user(
+            display_name=name,
+            email=email_id,
+            email_verified=True,
+            password=password
+        )
+        firestore_client.collection('users').document(user.uid).set({
+            'name': name,
+            'ownerOf': "",
+        })
+        print("Created user", user.uid)
+        return user.uid
+    except:
+        print("Error")
+        return None
+
+def createBusinessUserAccount(name,email_id, password,bname,category):
+    #try:
+        user = auth.create_user(
+            display_name=bname,
+            email=email_id,
+            email_verified=True,
+            password=password,
+        )
+        _,b_user = firestore_client.collection('businesses').add({
+            'name': bname,
+            'category':category,
+            'ownedBy': user.uid
+        })
+        firestore_client.collection('users').document(user.uid).set({
+            'name': name,
+            'ownerOf': b_user.id
+        })
+        print(b_user.id)
+        return user.uid
+    #except:
+    #    print("Error")
+    #    return None
+
+def loginUserAccount(email_id, password):
+    try:
+        user = auth.signInWithEmailAndPassword(
+            email=email_id,
+            password=password
+        )
+        return user.uid
+    except:
+        print("Error")
+        return None
     
      
